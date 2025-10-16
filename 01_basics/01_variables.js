bff05c4b88c59489da116667eba722467a17f427
CHUNNU KUMAR <chunnukumar013@gmail.com>
2025-10-16 19:16:29 +0000

    Add initial variable and data type examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountid=1235
+let accountEmail="alex@gmail.com"
+ accountCity="kolkata"
+var accountPassword=1589
+console.table([accountid,accountEmail,accountPassword,accountCity])
+
+
+//accountid=5
+accountEmail="cde@yahoo.com"
+accountPassword="vhuyjnd"
+accountCity="kalyani"
+console.table([accountid,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";// it will treat js as newer version
+// alert(3+3) // we are using nodejs not browser
+console.log(3+3)
+console.log("chunnu")
+
+let name="ayan"
+let age=23
+let isloggedin=false
+let state
+
+// number =>2 to power 53
+// boolean
+// string 
+// bigint
+// null=>standalone value
+// undefined
+//symbol=.symbols
+
+
+//object
+
+console.log(typeof"ayan")// string
+console.log(typeof age)// number
+console.log(typeof isloggedin)// boolean
+console.log(typeof state)// undefined
+console.log(typeof null)// object
+console.log(typeof undefined)//undefined
+console.log(typeof Object)//function
