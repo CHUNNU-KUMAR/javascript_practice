a61b642dc03dbbd09608ed692ee915f78e8fa640
CHUNNU KUMAR <chunnukumar013@gmail.com>
2025-10-19 19:59:55 +0530

    Add comparison operations examples in comparison.js
    Add comparison operations examples to demonstrate various comparison scenarios in JavaScript.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// comparison operations 
+console.log(2>3);
+console.log(2<3);
+console.log(8==10);
+console.log(9==9);
+
+console.log("10">=9);
+console.log("10"==10);
+console.log(10=="10");
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+//===
+console.log("2"==2);// no not equal because there will be no conversion
+
+
