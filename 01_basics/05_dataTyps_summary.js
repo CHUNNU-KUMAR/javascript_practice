3f72e398559eafa84a8c9d90ce3cd1095fb40e93
CHUNNU KUMAR <chunnukumar013@gmail.com>
2025-10-19 18:23:04 +0530

    Add summary of JavaScript data types
    This file summarizes the two types of data types in JavaScript: primitive and reference types, along with examples of each.

---
+++ 01_basics/05_dataTyps_summary.js
@@ -0,0 +1,36 @@
+// two types of data types
+//1 primitive types
+// it means the value of this will copied and then copied values will be changed not original value
+
+//7 types : string, number,boolean,null, undefined, symbol,Bigint
+
+const score = 100; // number
+const scorevalue = 100.3; // number
+
+const isLoggedIn = false; //boolean
+const outsideTemp = null; //object
+let userEmail;
+
+const id = Symbol("123"); // symbol
+const anotherId = Symbol("123"); //symbol
+console.log(id == anotherId);
+const bignumber = 35897885469585n; //bigint
+console.log(typeof bignumber);
+
+// 2.reference types or non-primitive types
+
+// array , objects , Functions
+
+const heros = ["ironman", "captain America", "thor", "loki"]; // object
+console.log(typeof heros);
+
+let myobj = {
+  name: "ayan",
+  age: 23,
+}; //object
+console.log(typeof myobj);
+
+const myfunction = function () {
+  console.log("hello world");
+}; //undefined
+console.log(typeof myFunction);
